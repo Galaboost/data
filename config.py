1403794a5169ee0812f3e57f8b9f137f8f2def6a
Galaboost <sukevin14@gmail.com>
2026-06-25 15:05:52 +0200

    up

--- config.py
+++ config.py
@@ -7,45 +7,33 @@ from sqlalchemy import create_engine
 load_dotenv()
 
 
-def connect_to_dmp():
-    url = os.environ.get("DMP_URL")
+def connect_to_reftool():
+    url = os.environ.get("REFTOOL_URL")
     if url:
         return create_engine(url, pool_pre_ping=True)
 
-    user = os.environ["DMP_USER"]
-    password = os.environ["DMP_PASSWORD"]
-    host = os.environ.get("DMP_HOST", "localhost")
-    port = os.environ.get("DMP_PORT", "4306")
-    database = os.environ.get("DMP_DATABASE", "dmp")
+    user = os.environ.get("REFTOOL_USER", "reftool_user")
+    password = os.environ.get("REFTOOL_PASSWORD", "reftool_user")
+    host = os.environ.get("REFTOOL_HOST", "reftooldb-dev.altissemiconductor.com")
+    port = os.environ.get("REFTOOL_PORT", "3306")
+    database = os.environ.get("REFTOOL_DATABASE", "reftool")
     return create_engine(
         f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}",
         pool_pre_ping=True,
     )
 
 
-def connect_to_dbtrade():
-    try:
-        import ibm_db_dbi
-    except ImportError as exc:
-        raise RuntimeError(
-            "DBTRADE requires the ibm-db package and its IBM DB2 runtime DLLs."
-        ) from exc
+def connect_to_dmp():
+    url = os.environ.get("REFTOOL_DMP_URL") or os.environ.get("DMP_URL")
+    if url:
+        return create_engine(url, pool_pre_ping=True)
 
-    connection_string = os.environ.get("DBTRADE_CONNECTION_STRING")
-    if connection_string:
-        return ibm_db_dbi.connect(connection_string, "", "")
-
-    database = os.environ["DBTRADE_DATABASE"]
-    host = os.environ["DBTRADE_HOST"]
-    port = os.environ.get("DBTRADE_PORT", "50000")
-    user = os.environ["DBTRADE_USER"]
-    password = os.environ["DBTRADE_PASSWORD"]
-    conn = (
-        f"DATABASE={database};"
-        f"HOSTNAME={host};"
-        f"PORT={port};"
-        "PROTOCOL=TCPIP;"
-        f"UID={user};"
-        f"PWD={password};"
+    user = os.environ.get("REFTOOL_DMP_USER", "appdatamart")
+    password = os.environ.get("REFTOOL_DMP_PASSWORD", "appdatamart1")
+    host = os.environ.get("REFTOOL_DMP_HOST", "maxscale")
+    port = os.environ.get("REFTOOL_DMP_PORT", "4306")
+    database = os.environ.get("REFTOOL_DMP_DATABASE", "dmp")
+    return create_engine(
+        f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}",
+        pool_pre_ping=True,
     )
-    return ibm_db_dbi.connect(conn, "", "")
